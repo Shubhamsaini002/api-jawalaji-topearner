3d8158dd077707e022c7e3a7088a13effcee55ae
Shubham saini <shubhamsainisk87@gmail.com>
2026-03-17 16:55:04 +0530

    complete code

--- emailapi/emailapi/Program.cs
+++ emailapi/emailapi/Program.cs
@@ -1,3 +1,6 @@
+using emailapi.Services;
+using EmailApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<EmailService>();
+builder.Services.AddSingleton<OtpService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
