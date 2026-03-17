056fb980da63ef4f084837dab145b8c7260d21f4
Shubham saini <shubhamsainisk87@gmail.com>
2026-03-17 18:43:10 +0530

    cor

--- emailapi/emailapi/Program.cs
+++ emailapi/emailapi/Program.cs
@@ -1,10 +1,19 @@
-using emailapi.Services;
+﻿using emailapi.Services;
 using EmailApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular",
+        policy =>
+        {
+            policy.AllowAnyOrigin()   // ⚠️ for development
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -12,7 +21,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<EmailService>();
 builder.Services.AddSingleton<OtpService>();
 var app = builder.Build();
-
+app.UseCors("AllowAngular");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
